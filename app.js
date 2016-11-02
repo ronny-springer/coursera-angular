be5be9800352bd60d9367cc3a46a8b1ad4dfdd35
Ronny Springer <springer.ronny@gmail.com>
2016-11-02 16:04:08 +0100

    lecture 6

--- app.js
+++ app.js
@@ -3,13 +3,25 @@
 	'use strict';
 
 	angular
-		.module('myFirstApp', [])
-		.controller('MyFirstController', function ($scope) {
+		.module('NameCalculator', [])
+		.controller('NameCalculatorController', function ($scope) {
+			$scope.name = "";
+			$scope.totalValue = 0;
 
-			$scope.name = "Ronny";
-			$scope.sayHello = function () {
-				return "Hallo Cousera";
-			};
+			$scope.displayNumberic = function () {
+				$scope.totalValue = calculateNumeric($scope.name);
+			}
+
+
+			function calculateNumeric (string) {
+				var totalStringValue = 0;
+
+				for (var i = 0; i < string.length; i++) {
+					totalStringValue += string.charCodeAt(i);
+				}
+
+				return (totalStringValue);
+			}
 		});
 
 })();
